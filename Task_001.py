8aa334d573d6a50e839fb745c0ec02f239b72fb1
Andrey <andrejsatykov@yandex.ru>
2023-02-04 15:00:50 +0300

    Coins

--- Task_001.py
+++ Task_001.py
@@ -6,14 +6,17 @@
 # 5 -> 1 0 1 1 0
 # 2
 
+from random import randint
 n = int(input('Введите количество монет лежащих на столе: '))
-print('Введите положение монеты 1 - решка, 0 - орел: ')
+# print('Введите положение монеты 1 - решка, 0 - орел: ')
 k = 0
 coin_position = []
 for i in range(n):    
-    v = int(input())
-    coin_position.append(v)
-    if v == 1:
-        k += 1
+    coin_position.append(randint(0, 1))
 print(n, "->", coin_position)
-print('Минимальное количество монет = ', k if k<n/2 else n-k)
+null = coin_position.count(0) 
+if len(coin_position) - null < null:
+    print(len(coin_position) - null)
+else:
+     print(null)
+
